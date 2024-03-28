d3f97f0025c4c1760e85d0efc8399b4a065783e7
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-03-28 16:53:10 -0500

    we're getting things loaded

--- scripts.js
+++ scripts.js
@@ -1,50 +1,55 @@
-// script.js
-
 $(document).ready(function() {
+    function showLoader() {
+        $(".loader").show();
+    }
 
-    
+    function hideLoader() {
+        $(".loader").hide();
+    }
+
     function fetchQuotes() {
-        var quoteCarousel = document.querySelector("#quote-carousel .carousel-inner");
-        var loader = document.createElement("div");
-        loader.className = "loader";
-        quoteCarousel.appendChild(loader);
+        var quoteCarousel = $(".slick-carousel");
+        showLoader();
 
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://smileschool-api.hbtn.info/quotes");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    var response = JSON.parse(xhr.responseText);
-                    quoteCarousel.innerHTML = ""; // Clear loader and existing quotes
-                    response.forEach(function (quote, index) {
-                        var quoteItem = document.createElement("div");
-                        quoteItem.className = "carousel-item" + (index === 0 ? " active" : "");
-                        quoteItem.innerHTML = `
-                            <div class="row align-items-center mx-auto">
-                                <div class="col-md-8 mx-auto">
-                                    <div class="testimonial-item">
-                                        <img src="${quote.pic_url}" class="testimonial-img" alt="Testimonial">
-                                        <h3>${quote.name}</h3>
-                                        <p>${quote.text}</p>
-                                    </div>
+        $.ajax({
+            url: "https://smileschool-api.hbtn.info/quotes",
+            method: "GET",
+            success: function(response) {
+                hideLoader();
+                quoteCarousel.empty(); // Clear loader and existing quotes
+                $.each(response, function(index, quote) {
+                    var quoteItem = $("<div>").addClass("carousel-item");
+                    var quoteContent = `
+                        <div class="row align-items-center mx-auto">
+                            <div class="col-md-8 mx-auto">
+                                <div class="testimonial-item">
+                                    <img src="${quote.pic_url}" class="testimonial-img" alt="Testimonial">
+                                    <h3>${quote.name}</h3>
+                                    <p>${quote.text}</p>
                                 </div>
-                            </div>`;
-                        quoteCarousel.appendChild(quoteItem);
-                    });
-                } else {
-                    console.error("Failed to fetch quotes. Status code: " + xhr.status);
-                }
+                            </div>
+                        </div>`;
+                    quoteItem.html(quoteContent);
+                    quoteCarousel.append(quoteItem);
+                });
+
+                quoteCarousel.slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    arrows: true,
+                    prevArrow: '<a class="carousel-control-prev arrow-left" href="#"><img src="images/arrow_white_left.png" alt="Quote Previous" aria-hidden="true"/><span class="sr-only">Previous</span></a>',
+                    nextArrow: '<a class="carousel-control-next arrow-right" href="#"><img src="images/arrow_white_right.png" alt="Quote Next" aria-hidden="true"/><span class="sr-only">Next</span></a>'
+                });
+            },
+            error: function(xhr, status, error) {
+                hideLoader();
+                console.error("Failed to fetch quotes. Status code: " + xhr.status);
             }
-        };
-        xhr.send();
+        });
     }
 
     // Fetch quotes when the page loads
     fetchQuotes();
 });
-
-$(document).ready(function(){
-    $('.carousel-inner"').slick({
-
-    });
-  });
